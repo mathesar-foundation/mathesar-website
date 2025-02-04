f585e7409ce6e30290c13329f53f0ff842f0b26d
zackdotcat <git@zack.cat>
2025-02-04 14:41:32 -0500

    Add tag for RSS feed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,6 +17,13 @@
 <svelte:head>
   <title>Mathesar</title>
 
+  <link
+    rel="alternate"
+    type="application/rss+xml"
+    title="Mathesar Blog - The latest news and updates from the Mathesar team."
+    href="https://mathesar.org/rss.xml"
+  />
+
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
