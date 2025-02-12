51766da8af693b548da496ca43c3736743ab4352
zackdotcat <git@zack.cat>
2025-02-12 14:04:58 -0500

    Remove webp github avatars; service used was marked as spam by Cloudflare

--- src/lib/components/ContributorLink.svelte
+++ src/lib/components/ContributorLink.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
   import type { Contributor } from "$lib/util/getContributors.server";
   export let contributor: Contributor;
-
-  // Optimize avatars
-  const optimizedURL = contributor.profile_image
-    .replace("https://github.com", "https://avatars-githubusercontent.webp.se")
-    .replace(".png", "");
 </script>
 
 <a href={contributor.profile_url}>
@@ -13,7 +8,7 @@
     loading="lazy"
     width={90}
     height={90}
-    src={optimizedURL}
+    src={contributor.profile_image}
     alt={`${contributor.name}'s avatar`}
     class="flex items-center justify-center text-transparent aspect-square text-center text-xs h-full w-full rounded-full shadow-lg transition-transform hover:scale-110 bg-gradient-to-tr from-plum-500 to-salmon-500"
   />
